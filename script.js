bcbcb5317cd876778a2315956bab6e62c8314ae0
Trend20 <enockomondi305@gmail.com>
2020-08-18 09:21:30 -0400

    modal

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ joinBtn.addEventListener('click', showModal);
 function showModal(){
     document.querySelector('#modal').style.visibility = 'visible';
 }
+
+const response = document.getElementById('response');
+response.addEventListener('click', hideModal);
+
+const hideModal = () =>{
+    document.querySelector('#modal').style.display = 'none';
+}
